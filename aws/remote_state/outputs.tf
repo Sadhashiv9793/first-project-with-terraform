924b1b816633c5a95205d4939c0cbdb96442dba4
Sadhashiv9793 <sadhagoud961821@gmail.com>
2026-09-11 00:13:56 +0530

    s3 and dynamoDb created with terraform file and also main tf file changes made

--- aws/remote_state/outputs.tf
+++ aws/remote_state/outputs.tf
@@ -1,24 +1,24 @@
 output "s3_bucket_name" {
   value       = aws_s3_bucket.terraform_state.id
-  description = "The NAME of the S3 bucket"
+  description = "Name of the Terraform state S3 bucket"
 }
 
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
+  description = "ARN of the Terraform state S3 bucket"
 }
 
 output "s3_bucket_region" {
   value       = aws_s3_bucket.terraform_state.region
-  description = "The REGION of the S3 bucket"
+  description = "Region of the Terraform state S3 bucket"
 }
 
 output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_lock.name
-  description = "The ARN of the DynamoDB table"
+  description = "Name of the Terraform lock table"
 }
 
 output "dynamodb_table_arn" {
   value       = aws_dynamodb_table.terraform_lock.arn
-  description = "The ARN of the DynamoDB table"
+  description = "ARN of the Terraform lock table"
 }
