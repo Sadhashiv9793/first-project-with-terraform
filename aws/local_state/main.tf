fa989d01ddd8c200df27557f2a312066e9b7ed14
Sadhashiv9793 <sadhagoud961821@gmail.com>
2026-09-11 00:14:14 +0530

    created s3 and dynamoDb details added

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,23 +2,51 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "196591915748-terraform-states"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-south-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "ap-south-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
